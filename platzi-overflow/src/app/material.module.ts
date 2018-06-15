1061109e19f3f31d1a29fe917ccdb1bb79aab87b
joosses <joosses@alumnos.ubiobio.cl>
2018-06-14 21:31:58 -0400

    Esto este es el proyecto que me da errores

---
+++ platzi-overflow/src/app/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from'@angular/core';
+import { MatToolbarModule } from'@angular/material/toolbar';
+import { MatIconModule } from'@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules
+ })
+
+ export class MaterialModule {}
